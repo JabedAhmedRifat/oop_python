90e6946942ae943cb5107e23353a86facb233e2f
JabedAhmedRifat <jabedahmedrifat1@gmail.com>
2024-03-07 19:21:25 +0600

    super()

--- super_keyword.py
+++ super_keyword.py
@@ -1,3 +1,7 @@
+#-----------------using Super() keyword to involve parent's methods and constructors---------------------- 
+# using super only can access parent's methods and constructors
+# can not access parent's attributes or data or variables 
+
 class Phone:
 
     def __init__(self, price,brand):
@@ -13,9 +17,16 @@ class SmartPhone(Phone):
 
     def buy(self):
         print("Buying a smart phone")
-        super().buy()
+        super().buy() #calling parent's method using super 
 
 
 s = SmartPhone(20000, "Apple", 13)
 
 s.buy()
+
+# s.super().buy() # can not call super outside of a child class
+
+
+# => Buying a smart phone
+# => Buying a phone
+
